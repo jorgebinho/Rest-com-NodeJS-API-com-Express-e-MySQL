965e6f0449a568e208604e7931cfb5c72e7840f2
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-01-31 15:26:44 -0300

    Criando primeira rota com arquivos isolados

---
+++ config/customExpress.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const consign = require('consign');
+
+const app = express();
+
+module.exports = () => {
+    consign()
+        .include('controllers')
+        .into(app)
+
+    return app;
+}
